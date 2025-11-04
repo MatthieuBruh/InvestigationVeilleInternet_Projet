f185c9c2e713d5388dac6777dfbb17e42c2a2d3d
MatthieuBruh <94464489+MatthieuBruh@users.noreply.github.com>
2025-11-04 22:02:32 +0100

    Linking main general to main le20minutes

--- scraper/main.py
+++ scraper/main.py
@@ -1,5 +1,6 @@
 from dbConfig import get_connection, close_connection
-
+from le20minutes.minutes_main import start_scraping as start_scraping_minutes
+from lematin.matin_main import start_scraping as start_scraping_matin
 
 def init_database():
     """Initialise la base de données SQLite"""
@@ -38,6 +39,8 @@ if __name__ == '__main__':
     try:
         # Lancer le scraping
         print("Commencement")
+        start_scraping_minutes()
+        start_scraping_matin()
         # TODO : AJOUTER LES SCRAPPER start_scraping()
     except KeyboardInterrupt:
         print("\n\n⚠️ Interruption utilisateur détectée")
